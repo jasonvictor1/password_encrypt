15b741428e3133d1d41c7c57a7231aea15e37f24
Jason Victor <141011930+jasonvictor1@users.noreply.github.com>
2023-11-04 03:04:37 -0700

    code updated with comments

--- encrypt_pass.py
+++ encrypt_pass.py
@@ -1,9 +1,18 @@
 import base64
 
+# Define a function to encrypt a password using base64 encoding.
 def encrypt_pass(password):
-    encode_bytes = base64.b64encode(password.encode())
-    print(encode_bytes)  # Corrected variable name here
+    # Convert the password string to bytes. 
+    password_bytes = password.encode()
+    
+    # Encode these bytes using base64 to get an encoded bytes object.
+    encode_bytes = base64.b64encode(password_bytes)
+    
+    # Print the base64 encoded bytes. This is the 'encrypted' password.
+    print(encode_bytes)
 
+# Prompt the user to enter their password.
 user_pass = input("Enter your password: ")
-encrypt_pass(user_pass)
 
+# Call the encrypt_pass function with the user's password to encrypt it.
+encrypt_pass(user_pass)
